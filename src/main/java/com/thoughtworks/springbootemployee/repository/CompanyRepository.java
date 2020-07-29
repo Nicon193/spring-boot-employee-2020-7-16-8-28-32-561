9bc25004d73f2431da43e29a58315acae3cc0bf6
Albert <Albert.c.x.chen@oocl.com>
2020-07-29 22:32:44 +0800

    [Albert] refactor:  change CompanyRepository from to interface

--- src/main/java/com/thoughtworks/springbootemployee/repository/CompanyRepository.java
+++ src/main/java/com/thoughtworks/springbootemployee/repository/CompanyRepository.java
@@ -1,34 +1,11 @@
 package com.thoughtworks.springbootemployee.repository;
 
 import com.thoughtworks.springbootemployee.model.Company;
-import com.thoughtworks.springbootemployee.model.Employee;
-
-import java.util.List;
-
-public class CompanyRepository {
-
-    public List<Company> findAll(){
-        return null;
-    }
+import org.springframework.data.jpa.repository.JpaRepository;
 
 
-    public Company findCompanyByID(int id) {
-        return null;
-    }
+public interface CompanyRepository extends JpaRepository<Company,Integer> {
 
-    public List<Employee> findCompanyEmployeesByID(int companyID) {
-        return null;
-    }
 
-    public List<Company> findRangeOfCompany(int page, int pageSize) {
-        return null;
-    }
 
-    public Company addCompany() {
-        return  null;
-    }
-
-    public Company deleteCompany(int companyID) {
-        return null;
-    }
 }
