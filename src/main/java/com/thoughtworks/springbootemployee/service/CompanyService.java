e3a200eb87d8ea42e6f74743d579e5135c4f3bfe
Albert <Albert.c.x.chen@oocl.com>
2020-07-29 17:42:26 +0800

    [Albert&York] feature: add should_return_company_employee_list_when_find_company_employee_list_by_id_given_company_id

--- src/main/java/com/thoughtworks/springbootemployee/service/CompanyService.java
+++ src/main/java/com/thoughtworks/springbootemployee/service/CompanyService.java
@@ -22,6 +22,6 @@ public class CompanyService {
     }
 
     public List<Employee> findCompanyEmployeesByID(int companyID) {
-        return null;
+        return companyRespository.findCompanyEmployeesByID(companyID);
     }
 }
