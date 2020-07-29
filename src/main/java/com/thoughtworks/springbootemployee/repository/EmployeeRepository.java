7f54ea2d28860d2e76f7eb296c8c42e22975a45d
Albert <Albert.c.x.chen@oocl.com>
2020-07-29 22:23:09 +0800

    [Albert] refactor:  refactor findEmployeeByGender method

--- src/main/java/com/thoughtworks/springbootemployee/repository/EmployeeRepository.java
+++ src/main/java/com/thoughtworks/springbootemployee/repository/EmployeeRepository.java
@@ -2,10 +2,16 @@ package com.thoughtworks.springbootemployee.repository;
 
 import com.thoughtworks.springbootemployee.model.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 
 @Repository
 public interface EmployeeRepository extends JpaRepository<Employee,Integer> {
+    @Query("select * from employ where gender = {gender}")
+    public List<Employee> findEmployeeByGender(String gender);
+
 
 }
