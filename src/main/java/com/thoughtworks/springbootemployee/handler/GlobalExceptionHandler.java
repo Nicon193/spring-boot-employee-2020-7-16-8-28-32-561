1ef02cd9cc824d11caa82557d9e2559e90b2df73
Albert <Albert.c.x.chen@oocl.com>
2020-08-02 17:43:17 +0800

    [Albert] feature: complete GlobalExceptionHandler class

--- src/main/java/com/thoughtworks/springbootemployee/handler/GlobalExceptionHandler.java
+++ src/main/java/com/thoughtworks/springbootemployee/handler/GlobalExceptionHandler.java
@@ -1,5 +1,30 @@
 package com.thoughtworks.springbootemployee.handler;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
+@ControllerAdvice
 public class GlobalExceptionHandler {
+
+
+    public static final String NO_SUCH_DATA = "There is no such data";
+    public static final String ERROR_OPERATION = "Error operation";
+
+    @ExceptionHandler(NoSuchDataException.class)
+    @ResponseBody
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    String noSuchDataException() {
+        return NO_SUCH_DATA;
+    }
+
+    @ExceptionHandler(ErrorOperationException.class)
+    @ResponseBody
+    @ResponseStatus(HttpStatus.FORBIDDEN)
+    String errorOperationException() {
+        return ERROR_OPERATION;
+    }
+
 }
