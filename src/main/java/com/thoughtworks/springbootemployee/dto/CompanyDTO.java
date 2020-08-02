3dd9c28d9b94411a88cf2c61149965a36e780918
Albert <Albert.c.x.chen@oocl.com>
2020-08-02 23:07:14 +0800

    [Albert] feature: add DTO and Mapper class

---
+++ src/main/java/com/thoughtworks/springbootemployee/dto/CompanyDTO.java
@@ -0,0 +1,49 @@
+package com.thoughtworks.springbootemployee.dto;
+
+import com.thoughtworks.springbootemployee.model.Employee;
+
+import java.util.List;
+
+
+public class CompanyDTO {
+
+    private Integer id;
+    private String companyName;
+    private Integer employeesNumber;
+    List<Employee> employees;
+
+    public CompanyDTO() {
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getCompanyName() {
+        return companyName;
+    }
+
+    public void setCompanyName(String companyName) {
+        this.companyName = companyName;
+    }
+
+    public Integer getEmployeesNumber() {
+        return employeesNumber;
+    }
+
+    public void setEmployeesNumber(Integer employeesNumber) {
+        this.employeesNumber = employeesNumber;
+    }
+
+    public List<Employee> getEmployees() {
+        return employees;
+    }
+
+    public void setEmployees(List<Employee> employees) {
+        this.employees = employees;
+    }
+}
---
+++ src/main/java/com/thoughtworks/springbootemployee/mapper/CompanyMapper.java
@@ -0,0 +1,8 @@
+package com.thoughtworks.springbootemployee.mapper;
+
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class CompanyMapper {
+}
