27f09c92e16f719c52597c24ac64dea5df3fe6ca
Albert <Albert.c.x.chen@oocl.com>
2020-08-02 23:08:49 +0800

    [Albert] feature: add  Mapper class

--- src/main/java/com/thoughtworks/springbootemployee/mapper/CompanyMapper.java
+++ src/main/java/com/thoughtworks/springbootemployee/mapper/CompanyMapper.java
@@ -1,8 +1,23 @@
 package com.thoughtworks.springbootemployee.mapper;
 
 
+import com.thoughtworks.springbootemployee.dto.CompanyDTO;
+import com.thoughtworks.springbootemployee.model.Company;
+import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Component;
 
 @Component
 public class CompanyMapper {
+
+    public CompanyDTO switchEntityToDto(Company company) {
+        CompanyDTO companyDTO = new CompanyDTO();
+        BeanUtils.copyProperties(company, companyDTO);
+        return companyDTO;
+    }
+
+    public Company switchDtoToEntity(CompanyDTO companyDTO) {
+        Company company = new Company();
+        BeanUtils.copyProperties(companyDTO, company);
+        return company;
+    }
 }
--- src/main/java/com/thoughtworks/springbootemployee/mapper/EmployeeMapper.java
+++ src/main/java/com/thoughtworks/springbootemployee/mapper/EmployeeMapper.java
@@ -1,8 +1,23 @@
 package com.thoughtworks.springbootemployee.mapper;
 
 
+import com.thoughtworks.springbootemployee.dto.EmployeeDTO;
+import com.thoughtworks.springbootemployee.model.Employee;
+import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Component;
 
 @Component
 public class EmployeeMapper {
+
+    public EmployeeDTO switchEntityToDto(Employee employee) {
+        EmployeeDTO employeeDTO = new EmployeeDTO();
+        BeanUtils.copyProperties(employee, employeeDTO);
+        return employeeDTO;
+    }
+
+    public Employee switchDtoToEntity(EmployeeDTO employeeDTO) {
+        Employee employee = new Employee();
+        BeanUtils.copyProperties(employeeDTO,employee);
+        return employee;
+    }
 }
